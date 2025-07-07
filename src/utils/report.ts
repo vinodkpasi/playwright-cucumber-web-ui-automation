78b1e147911524973553dc8d8aa7897c26238557
vinodkpasi <vinodkpasi@gmail.com>
2025-07-08 00:07:37 +0530

    Update report.ts

--- src/utils/report.ts
+++ src/utils/report.ts
@@ -4,7 +4,7 @@ const report = require("multiple-cucumber-html-reporter");
 report.generate({
   jsonDir: "test-results",
   reportPath: "test-results/reports/",
-  reportName: "SkillCycle Test Report",
-  pageTitle: "SkillCycle Test Report",
+  reportName: "QA Automation Test Report",
+  pageTitle: "QA Automation Test Report",
   displayDuration: false,
 });
